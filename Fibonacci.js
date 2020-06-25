e447eb908c203bf3de93c771e537f0deae2357da
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2020-06-25 11:10:03 -0400

    1
    1

---
+++ Fibonacci.js
@@ -0,0 +1,16 @@
+function fib(n)
+{
+    if (n <= 1)
+     {
+        return n;
+     }
+    else
+    {
+      return fib(n-1) + fib(n-2);
+    }
+}
+
+for (var i = 0; i <= 10; i += 1) {
+    document.writeln('// ' + i + ': ' + fib(i));
+}
+
